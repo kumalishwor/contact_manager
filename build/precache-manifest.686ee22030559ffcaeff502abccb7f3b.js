c5144812baf70765414e20e232aafce65f22b6ab
Ishwor kumal <Ishworkumal58530@gmail.com>
2020-07-16 14:49:33 +0545

    first commit

---
+++ build/precache-manifest.686ee22030559ffcaeff502abccb7f3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1721b7d3722e07d05f7fa76670e38da0",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "13c09e71fe49b780b884",
+    "url": "/ContactManager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "98b41738c517cccc5f3b",
+    "url": "/ContactManager/static/css/main.09d3c97a.chunk.css"
+  },
+  {
+    "revision": "13c09e71fe49b780b884",
+    "url": "/ContactManager/static/js/2.0b0f65ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.0b0f65ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b41738c517cccc5f3b",
+    "url": "/ContactManager/static/js/main.62ea2ef8.chunk.js"
+  },
+  {
+    "revision": "cbd985fc1d3bd3f56dc3",
+    "url": "/ContactManager/static/js/runtime-main.c05a890c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ContactManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
