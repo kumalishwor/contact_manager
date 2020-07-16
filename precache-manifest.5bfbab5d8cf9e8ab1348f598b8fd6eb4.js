8406fdd205b41afd169f4a4f5e5f01171fb543c7
Ishwor kumal <Ishworkumal58530@gmail.com>
2020-07-16 14:58:11 +0545

    Updates

---
+++ precache-manifest.5bfbab5d8cf9e8ab1348f598b8fd6eb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f11e8db4d42cc974251591384ce29b6",
+    "url": "/contact_manager/index.html"
+  },
+  {
+    "revision": "13c09e71fe49b780b884",
+    "url": "/contact_manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "05aabf5cbf97f14394a3",
+    "url": "/contact_manager/static/css/main.09d3c97a.chunk.css"
+  },
+  {
+    "revision": "13c09e71fe49b780b884",
+    "url": "/contact_manager/static/js/2.0b0f65ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact_manager/static/js/2.0b0f65ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05aabf5cbf97f14394a3",
+    "url": "/contact_manager/static/js/main.21d15d08.chunk.js"
+  },
+  {
+    "revision": "fbe5f327c084eb138ba0",
+    "url": "/contact_manager/static/js/runtime-main.86c9167d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contact_manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
